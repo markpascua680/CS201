376d8267b303a0d93b20e754ba00cbc576542e93
jpascua3 <jpascua3@alaska.edu>
2020-01-17 10:03:34 -0900

    Added poem and game desc.

--- hw0/hw0/hw0.cpp
+++ hw0/hw0/hw0.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
+void main(int argc, char** argv)
 {
 	for (int i = 0; i < 5; i++)
 	{
 		std::cout << "Hello, world!" << std::endl;
 	}
-	return 0;
+	
+	std::cout << "\nPoem Excerpt:" << std::endl;
+	std::cout << "Last night I saw a turtle;\nIt looked like a grenade.\nI picked it up and threw it.\nNo explosion was made.\n" << std::endl;
+
+	std::cout << "Favorite Game Description:" << std::endl;
+	std::cout << "League of Legends is a fast-paced, competitive online game that blends the speed and intensity of an RTS with RPG elements. Two teams of powerful champions, each with a unique design and playstyle, battle head-to-head across multiple battlefields and game modes" << std::endl;
+
 }
