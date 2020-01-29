1091e220a3892ad8d6162b36174854c82a85e5ed
Jay-Mark Pascua <jpascua3@alaska.edu>
2020-01-29 13:59:15 -0900

    Successful pyramid without spaces

--- Homework1/Diamond.cpp
+++ Homework1/Diamond.cpp
@@ -6,7 +6,7 @@ void main()
 {
 	int n = 0; // n = user's input
 
-	while (n==n) // Test for postive number
+	while (n == n) // Test for postive number
 	{
 		cout << "Enter a positive number: ";
 		std::cin >> n;
@@ -18,12 +18,41 @@ void main()
 			break;
 	}
 
-	int row = 0;
-	for (int i=1; i <= n; i++)
-	{	
+	int row = 0; // Adds a # to each successive row
+
+	int pound = 0; // Indicates how many #'s to output
+
+	for (int i = 1; i <= n; i++) // Creates the rows of the diamond up to the width
+	{
 		
-		cout << i + row << endl;
+		for (pound = pound; pound >= 0; pound--)
+		{
+			cout << "#";
+		}
+		cout << endl;
+
 		row++;
+		pound = i + row;
+		
+
+		if (i == n) // Tests if the max width is met; n = rows until max width
+		{
+			row-=2; // Prevents program from reprinting the max width twice
+
+			for (int i=n-1; i > 0; i--) // Creates the rows after the diamond's width
+			{
+				
+				pound = i + row;
+				for (pound = pound; pound > 0; pound--)
+				{
+					cout << "#";
+				}
+				cout << endl;
+				row--;
+
+
+			}
+		}
 
 	}
 
