a57e2596b28fdb94e1ccceb8063eed08cd29ea29
Jay-Mark Pascua <jpascua3@alaska.edu>
2020-01-29 10:09:39 -0900

    Added test for positive number

---
+++ Homework1/Diamond.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void main()
+{
+	int n = 0; // n = user's input
+
+	while (n==n) // Test for postive number
+	{
+		cout << "Enter a positive number: ";
+		std::cin >> n;
+		if (n < 0)
+			cout << "That is not a positive number!\n" << endl;
+		else if (n == 0)
+			cout << "The number cannot be 0.\n" << endl;
+		else
+			break;
+		
+	}
+
+
+}
