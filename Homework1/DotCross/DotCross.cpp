f6d416cd646b7ccbcd786e06823b6327c955f7ec
Jay-Mark <jpascua3@alaska.edu>
2020-01-31 10:08:35 -0900

    Test

---
+++ Homework1/DotCross/DotCross.cpp
@@ -0,0 +1,22 @@
+/* DotCross.cpp
+   Jay-Mark Pascua
+   CS 201
+   01/31/2020
+   Calculates the dot product and cross product of a 3-component vector
+*/
+
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::setprecision;
+
+int main()
+{
+
+
+
+
+    return 0;
+}
