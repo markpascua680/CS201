9e3398c1be32d27f41eac92416b497d1dcebd554
Jay-Mark <jpascua3@alaska.edu>
2020-02-10 09:51:22 -0900

    Created main function shell

---
+++ Homework 2/Money/Source.cpp
@@ -0,0 +1,30 @@
+/* Jay-Mark Pascua
+   CS 201
+   02/10/2020
+   User enters number of pennies, nickels, dimes, quarters, and dollar coins. Outputs total.
+*/
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int pennies;
+int nickels;
+int dimes;
+int quarters;
+int dollars;
+int total;
+
+int main()
+{
+    cout << "How many pennies do you have?" << endl;
+    cout << "How many nickels do you have?" << endl;
+    cout << "How many dimes do you have?" << endl;
+    cout << "How many quarters do you have?" << endl;
+    cout << "How many dollar coins do you have?" << endl;
+
+
+
+    return 0;
+}
