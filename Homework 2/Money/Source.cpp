c6757474637e7e5d0c34a03821f5e06b37ac9e04
Jay-Mark <jpascua3@alaska.edu>
2020-02-10 10:05:09 -0900

    Added function that prints the amount of each coin

--- Homework 2/Money/Source.cpp
+++ Homework 2/Money/Source.cpp
@@ -16,15 +16,31 @@ int quarters;
 int dollars;
 int total;
 
+void printCoins(int p, int n, int d, int q, int dol);
+
 int main()
 {
     cout << "How many pennies do you have?" << endl;
+        cin >> pennies;
     cout << "How many nickels do you have?" << endl;
+        cin >> nickels;
     cout << "How many dimes do you have?" << endl;
+        cin >> dimes;
     cout << "How many quarters do you have?" << endl;
+        cin >> quarters;
     cout << "How many dollar coins do you have?" << endl;
+        cin >> dollars;
 
-
+        printCoins(pennies, nickels, dimes, quarters, dollars);
 
     return 0;
+}
+
+void printCoins(int p, int n, int d, int q, int dol)
+{
+    cout << "You have " << p << " pennies" << endl;
+    cout << "You have " << n << " nickels" << endl;
+    cout << "You have " << d << " dimes" << endl;
+    cout << "You have " << q << " quarters" << endl;
+    cout << "You have " << dol << " dollars" << endl;
 }
