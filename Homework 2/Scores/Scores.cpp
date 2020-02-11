3cac1bdf45a2daaee33df0b67b4ea02c844680ab
Jay-Mark <jpascua3@alaska.edu>
2020-02-10 15:04:38 -0900

    Added option 1 and 2

--- Homework 2/Scores/Scores.cpp
+++ Homework 2/Scores/Scores.cpp
@@ -18,21 +18,30 @@ vector<string> names;
 vector<string> scores;
 
 void namesNumbers();
+void selectOption();
+void printNamesScores();
+void searchName();
+void searchScore();
+
+int option; // The option chosen by user
+int flag = 0;   // Stops while loop in namesNumbers
 
 int main()
 {
-    
     cout << "Enter NoName and 0 to stop\n" << endl;
     namesNumbers();
-
+    selectOption();
 
 
     return 0;
 }
 
+
+
+
+
 void namesNumbers()
 {
-    int flag = 0;   // Stops while loop in namesNumbers
     while (flag == 0)
     {
         string name;
@@ -41,7 +50,10 @@ void namesNumbers()
         cout << "Enter name: ";
         cin >> name;
 
-        if (names.size() >= 1)
+        cout << "Enter score: ";
+        cin >> num;
+
+        if (names.size() >= 1) // Checks if names vector has more than one name
         {
             for (int i = 0; i < names.size(); i++) // Stops program if a name is entered twice
             {
@@ -58,9 +70,6 @@ void namesNumbers()
         {
             break;
         }
-
-        cout << "Enter score: ";
-        cin >> num;
         
         cout << "\n";
 
@@ -74,19 +83,73 @@ void namesNumbers()
             names.push_back(name);
             scores.push_back(num);
         }
-
         
+    }
+    
+}
 
 
+
+
+
+void selectOption()
+{
+    cout << "To add names and scores, enter 1." << endl;
+    cout << "To print the names and scores, enter 2." << endl;
+    cout << "To search for a name, enter 3." << endl;
+    cout << "To search for a score, enter 4." << endl;
+    cin >> option;
+
+    if (option == 1)
+    {
+        flag == 0;
+        namesNumbers();
+    }
+    else if (option == 2)
+    {
+        printNamesScores();
+    }
+    else if (option == 3)
+    {
 
     }
+    else if (option == 4)
+    {
 
-    /* cout << "List of names and scores:" << endl;
+    }
+                else
+                    cout << "Invalid number" << endl;
+}
 
+
+
+
+
+void printNamesScores()
+{
+    cout << "List of names and scores:" << endl;
     for (int i = 0; i < names.size(); i++)
     {
         cout << names[i] << " ";
         cout << scores[i];
         cout << endl;
-     */
+    }
+}
+
+
+
+
+
+void searchName()
+{
+
+}
+
+
+
+
+
+void searchScore()
+{
+
 }
