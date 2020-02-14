15e685c64c078dd71f2e0a83bff9ae12edf5b7e6
Jay-Mark <jpascua3@alaska.edu>
2020-02-13 17:37:02 -0900

    Created main shell and .cpp and .h files

---
+++ Homework 3/Boxer.cpp/Boxer.cpp
@@ -0,0 +1,12 @@
+/* Jay-Mark Pascua
+   CS 201
+   02/13/2020
+   Takes user's string and int input and prints a box of asterisks with the string in the center
+*/
+#include<iostream>
+
+int main()
+{
+
+    return 0;
+}
