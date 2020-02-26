38bbbf04c89e9c3333e0adb2e014eff8b76ae6cb
Jay-Mark <jpascua3@alaska.edu>
2020-02-25 22:35:51 -0900

    Implemented Bulls function: searches user's guess for correct numbers in correct position. With error check to prevent < 4 digits being entered

--- Homework 4/Bulls-And-Cows/Bulls-And-Cows.cpp
+++ Homework 4/Bulls-And-Cows/Bulls-And-Cows.cpp
@@ -11,17 +11,66 @@ using std::endl;
 using std::cin;
 using std::string;
 
+void Bulls(string guess);
+void Cows(string guess);
+
+string ans = "7420"; // Number answer
+string guess = ""; // User's guess
+
 int main()
 {
-    string ans = "7420"; // Number answer
-    string guess = ""; // User's guess
-
     cout << "!!!!!  BULLS AND COWS  !!!!!\nGuess the four digit number!" << endl;
 
     while (guess != ans) // Loops user's input so they can keep guessing
     {
         getline(cin, guess);
+
+        if (guess.size() < 4) // Error check
+        {
+            cout << "Please enter four digits!" << endl;
+        }
+
+        if (guess.size() == 4)
+        {
+            Bulls(guess);
+            Cows(guess);
+        }
     }
 
+    cout << "\nYou got four bulls!" << endl;
+
     return 0;
+}
+
+
+void Bulls(string guess) // Searches for correct numbers in correct position
+{
+    int bulls = 0;
+    for (int i = 0; i < 4; i++) // int i = elements of answer number and guessed number
+    {
+        if (ans.at(i) == guess.at(i))
+        {
+            bulls++;
+        }
+    }
+
+    if (bulls == 0)
+    {
+        cout << bulls << " bulls and ";
+    }
+    else if (bulls == 1)
+    {
+        cout << bulls << " bull and ";
+    }
+    else if (bulls > 1)
+    {
+        cout << bulls << " bulls and ";
+    }
+}
+
+
+
+void Cows(string guess)
+{
+    
 }
