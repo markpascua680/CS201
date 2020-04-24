5ea56e72c68796586a2ea2818bfea604781db81e
Jay-Mark <jpascua3@alaska.edu>
2020-04-23 17:27:42 -0800

    PolishedHangman.cpp: Declared multiple functions in hangman.h

--- Homework 8/Polished Hangman/hangman.h
+++ Homework 8/Polished Hangman/hangman.h
@@ -5,10 +5,36 @@
    Hangman header file
 */
 
+#include <map>
+
 #pragma once
 #ifndef HANGMAN_H
 #define HANGMAN_H
 
 
+std::string answer = "computer"; // Hangman answer
+std::map<char, char> blanks { // Displays blank spaces; will reveal letter if guessed correctly
+    {'c', '_'},
+    {'o', '_'},
+    {'m', '_'},
+    {'p', '_'},
+    {'u', '_'},
+    {'t', '_'},
+    {'e', '_'},
+    {'r', '_'}
+};
+int attempts = 10; // Attempts to guess
+
+char guess(); // Player guesses a letter
+
+void checkGuess(char& guess); // Check if letter guessed is correct/incorrect/used before
+
+void correct(char& guess); // Reveals letter according to blank spaces
+
+void incorrect(char& guess); // Tells player their guess is incorrect
+
+void hasBeenUsed(char& guess); // Tells player their guess has been used before
+
+bool gameOver(); // Stops game
 
 #endif 
